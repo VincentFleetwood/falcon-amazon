a2d53d711a2cc5b7113179cc4d1007ca30f0abd7
Edward Thompson Brooks <edward.thompsonbrooks@mail.bcu.ac.uk>
2023-04-28 10:12:54 +0100

    Basic Structure added
    Added the basic components of the database connection and customer classes. Added a startup class to the project so that the database context can be accessed using a connection string that is configured in appsettings.json.

---
+++ FalconAmazonRestAPI/Customer.cs
@@ -0,0 +1,10 @@
+﻿// Represents a Customer entity
+public class Customer
+{
+    public int? CustomerID { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? EmailAddress { get; set; }
+    public string? PhoneNumber { get; set; }
+    public DateTime? DOB { get; set; }
+}
