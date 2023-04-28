e9fe18e2bacd05f214b454837c8075d8959c6e08
Edward Thompson Brooks <edward.thompsonbrooks@mail.bcu.ac.uk>
2023-04-28 11:16:37 +0100

    Program.cs updated

--- FalconAmazonRestAPI/Program.cs
+++ FalconAmazonRestAPI/Program.cs
@@ -1,6 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+// Creates the application builder
 var builder = WebApplication.CreateBuilder(args);
+
+// Adds the database to the dependency injection container
+builder.Services.AddDbContext<IDatabaseContext, DatabaseContext>();
+
+// Add the customer service to the dependency injection container
+builder.Services.AddScoped<CustomerService>();
+
+// Builds the app
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Replaces repated routes for ease of use
+RouteGroupBuilder customers = app.MapGroup("/customers");
 
+// Creates the endpoint definitions
+customers.MapPost("/", AddNewCustomer);
+customers.MapGet("/", GetCustomerFromID);
+
+// Starts the API
 app.Run();
+
+
+static async Task<IResult> AddNewCustomer([FromBody] Customer customer, [FromServices] CustomerService customerService) 
+{
+    return Results.Ok();
+}
+
+static async Task<IResult> GetCustomerFromID(int customerID, [FromServices] CustomerService customerService)
+{
+    return Results.Ok();
+}
