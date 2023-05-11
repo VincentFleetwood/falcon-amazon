c870bd5a67472fac525874d05f2589c9674863a3
Amy Kernutt <amyxce@gmail.com>
2023-05-11 09:45:17 +0100

    updated create table scripts
    updated the user and address table to match requirement docs

--- SQL/create_tables.sql
+++ SQL/create_tables.sql
@@ -1,15 +1,5 @@
 USE FalconAmazonDB;
 
-CREATE TABLE tbl_addresses
-(
-	id INT NOT NULL IDENTITY PRIMARY KEY,
-	address_line_1 VARCHAR(200) NOT NULL,
-	address_line_2 VARCHAR(200) NULL,
-	city VARCHAR(100) NOT NULL,
-	postcode VARCHAR(20) NOT NULL,
-	country VARCHAR(50) NOT NULL,
-);
-
 CREATE TABLE tbl_users 
 (
     id INT NOT NULL IDENTITY PRIMARY KEY,
@@ -17,15 +7,29 @@ CREATE TABLE tbl_users
     password_hash VARCHAR(120) NOT NULL,
 	first_name VARCHAR(50) NOT NULL,
 	last_name VARCHAR(100) NOT NULL,
-	email VARCHAR(200) UNIQUE NOT NULL,
-	phone_number VARCHAR(15) UNIQUE NOT NULL,
-	dob DATE NOT NULL,
-	address_id INT NOT NULL,
+	email_address VARCHAR(200) UNIQUE NOT NULL,
+	telephone VARCHAR(15) UNIQUE NOT NULL,
+	date_of_birth DATETIME NOT NULL,
     date_added DATETIME NOT NULL,
     date_last_modified DATETIME NOT NULL,
 	failed_login_attempts INT NOT NULL,
     is_deleted BIT NOT NULL,
 
-	FOREIGN KEY (address_id) REFERENCES dbo.tbl_addresses(id)
+);
+
+CREATE TABLE tbl_addresses
+(
+	id INT NOT NULL IDENTITY PRIMARY KEY,
+	user_id INT NOT NULL,
+	house_name_number VARCHAR(200) NOT NULL,
+	street VARCHAR(200) NOT NULL,
+	address_line_2 VARCHAR(200) NULL,
+	address_line_3 VARCHAR(200) NULL,
+	city VARCHAR(100) NOT NULL,
+	postcode VARCHAR(20) NOT NULL,
+	country VARCHAR(50) NOT NULL,
+	is_business BIT NOT NULL,
+
+	FOREIGN KEY (user_id) REFERENCES dbo.tbl_users(id)
 );
 
